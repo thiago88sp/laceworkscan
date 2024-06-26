5aaa1b44c90996b2865d3cbfa35a1b6671bc2d17
Thiago Pontes <tpontes@fortinet.com>
2024-06-26 15:30:46 -0300

    MySQL Flexible Server

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -1,20 +1,11 @@
-resource "azurerm_mysql_server" "mysql" {
-  name                = "lwlab-mysqlserver"
-  location            = azurerm_resource_group.rsg.location
-  resource_group_name = azurerm_resource_group.rsg.name
+resource "azurerm_mysql_flexible_server" "mysql" {
+  name                   = "lw-msql-001"
+  resource_group_name    = azurerm_resource_group.rsg.name
+  location               = azurerm_resource_group.rsg.location
+  administrator_login    = "psqladmin"
+  administrator_password = "H@Sh1CoR3!"
+  backup_retention_days  = 1
 
-  administrator_login          = "mysqladminun"
-  administrator_login_password = "H@Sh1CoR3!"
+  sku_name               = "GP_Standard_D2ds_v4"
 
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
-
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
